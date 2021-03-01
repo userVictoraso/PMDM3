3d810b935552ce5cf38ba826faba51a5878b5186
userVictoraso <al.victor.pareja.ramirez@iesportada.org>
2021-03-01 09:34:10 +0100

    hasta aqui bien

--- app/src/main/java/com/example/pareja_ramirez_victor_datos/Ejercicio3/Clases/CrearRegistroActivity.java
+++ app/src/main/java/com/example/pareja_ramirez_victor_datos/Ejercicio3/Clases/CrearRegistroActivity.java
@@ -2,9 +2,12 @@ package com.example.pareja_ramirez_victor_datos.Ejercicio3.Clases;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.pareja_ramirez_victor_datos.databinding.ActivityCrearRegistroBinding;
 import com.example.pareja_ramirez_victor_datos.databinding.ActivityMain3Binding;
@@ -23,5 +26,108 @@ public class CrearRegistroActivity extends AppCompatActivity {
     public void registrar(View view) {
         SQLite admin = new SQLite(this, "myDataBase", null, 1);
         SQLiteDatabase db = admin.getWritableDatabase();
+
+        String nombre = binding.editTextName.getText().toString();
+        String link = binding.editTextLink.getText().toString();
+        String email = binding.editTextEmail.getText().toString();
+        String categoria = binding.editTextCategory.getText().toString();
+
+        if(!nombre.isEmpty() && !link.isEmpty() && !email.isEmpty() && !categoria.isEmpty()){
+            ContentValues registro = new ContentValues();
+            registro.put("nombre", nombre);
+            registro.put("link", link);
+            registro.put("email", email);
+            registro.put("categoria", categoria);
+
+            db.insert("web", null, registro);
+            db.close();
+            Toast.makeText(this, "Registro realizado", Toast.LENGTH_SHORT).show();
+            limpiarCampos();
+        } else {
+            Toast.makeText(this, "Rellena todos los campos", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void buscar(View view) {
+        SQLite admin = new SQLite(this, "myDataBase", null, 1);
+        SQLiteDatabase db = admin.getWritableDatabase();
+
+        String nombre = binding.editTextName.getText().toString();
+
+        if (!nombre.isEmpty()) {
+            Cursor cursor = db.rawQuery
+                    ("select link, email from web where nombre=" + "'" + nombre + "'", null);
+
+            if (cursor.moveToFirst()) {
+                binding.editTextLink.setText(cursor.getString(0));
+                binding.editTextEmail.setText(cursor.getString(1));
+                db.close();
+            } else {
+                Toast.makeText(this, "No existe", Toast.LENGTH_SHORT).show();
+                db.close();
+            }
+        }
+    }
+
+    public void eliminar(View view) {
+        SQLite admin = new SQLite(this, "myDataBase", null, 1);
+        SQLiteDatabase db = admin.getWritableDatabase();
+
+        String nombre = binding.editTextName.getText().toString();
+
+        if (!nombre.isEmpty()) {
+            int cantidad = db.delete("web", "nombre=" + "'" + nombre + "'", null);
+            db.close();
+
+            limpiarCampos();
+
+            if (cantidad == 1) {
+                Toast.makeText(this, "Web eliminada", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Web inexistente", Toast.LENGTH_SHORT).show();
+            }
+
+        } else {
+            Toast.makeText(this, "Introduce un nombre", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void modificar(View view) {
+        SQLite admin = new SQLite(this, "myDataBase", null, 1);
+        SQLiteDatabase db = admin.getWritableDatabase();
+
+
+        String nombre = binding.editTextName.getText().toString();
+        String link = binding.editTextLink.getText().toString();
+        String email = binding.editTextEmail.getText().toString();
+        String categoria = binding.editTextCategory.getText().toString();
+
+        if(!nombre.isEmpty() && !link.isEmpty() && !email.isEmpty() && !categoria.isEmpty()) {
+            ContentValues registro = new ContentValues();
+
+            registro.put("nombre", nombre);
+            registro.put("link", link);
+            registro.put("email", email);
+            registro.put("categoria", categoria);
+
+            int cantidad = db.update("web", registro, "nombre=" + "'" + nombre + "'", null);
+            db.close();
+
+            if(cantidad == 1) {
+                Toast.makeText(this, "Web modificada", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Rellena todos los campos", Toast.LENGTH_SHORT).show();
+            }
+
+        } else {
+            Toast.makeText(this, "Rellena todos los campos", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void limpiarCampos(){
+        binding.editTextName.setText("");
+        binding.editTextLink.setText("");
+        binding.editTextEmail.setText("");
+        binding.editTextCategory.setText("");
     }
 }
--- app/src/main/java/com/example/pareja_ramirez_victor_datos/Ejercicio3/MainActivity3.java
+++ app/src/main/java/com/example/pareja_ramirez_victor_datos/Ejercicio3/MainActivity3.java
@@ -2,10 +2,14 @@ package com.example.pareja_ramirez_victor_datos.Ejercicio3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 
+import com.example.pareja_ramirez_victor_datos.Ejercicio2.AlarmActivity.AlarmActivity;
+import com.example.pareja_ramirez_victor_datos.Ejercicio2.MainActivity2;
+import com.example.pareja_ramirez_victor_datos.Ejercicio3.Clases.CrearRegistroActivity;
 import com.example.pareja_ramirez_victor_datos.Ejercicio3.Clases.SQLite;
 import com.example.pareja_ramirez_victor_datos.databinding.ActivityMain3Binding;
 
@@ -17,6 +21,13 @@ public class MainActivity3 extends AppCompatActivity {
         binding = ActivityMain3Binding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+
+        binding.button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity3.this, CrearRegistroActivity.class));
+            }
+        });
     }
 
 
